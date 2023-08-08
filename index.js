b8a342bcd9fa626839eaaf2b1df5f06412f6a8a6
= <peterndirangu0005@gmail.com>
2023-08-08 20:26:16 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,94 @@
 /* Your Code Here */
 
+// Create an employee record
+function createEmployeeRecord(employeeInfo) {
+    return {
+        firstName: employeeInfo[0],
+        familyName: employeeInfo[1],
+        title: employeeInfo[2],
+        payPerHour: employeeInfo[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+// Create multiple employee records
+function createEmployeeRecords(employeesData) {
+    return employeesData.map(createEmployeeRecord);
+}
+
+// Create a time-in event
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    });
+    return this;
+}
+
+// Create a time-out event
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    });
+    return this;
+}
+
+// Calculate hours worked on a specific date
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+// Calculate wages earned on a specific date
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+// Calculate total wages for an employee
+function allWagesFor() {
+    const eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date;
+    });
+
+    const payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this), 0);
+
+    return payable;
+}
+
+// Find an employee record by first name
+function findEmployeeByFirstName(employees, firstName) {
+    return employees.find(employee => employee.firstName === firstName);
+}
+
+// Calculate total payroll cost for all employees
+function calculatePayroll(employees) {
+    return employees.reduce(function (total, employee) {
+        return total + allWagesFor.call(employee);
+    }, 0);
+}
+
+module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    findEmployeeByFirstName,
+    calculatePayroll
+};
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
